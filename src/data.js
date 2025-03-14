05ff14c197fa2833251bd532bd9585242f26d28c
sandesh4747 <sangitthapa328@gmail.com>
2025-03-14 07:38:02 +0545

    changes

---
+++ src/data.js
@@ -0,0 +1,21 @@
+export const data = [
+  {
+    id: 1,
+    name: "Miguel",
+    lastName: "Perez",
+    age: 25,
+    gender: "male",
+    country: "Colombia",
+    city: "Bogota",
+  },
+  {
+    id: 2,
+    name: "Maria",
+    lastName: "Perez",
+    age: 25,
+    gender: "female",
+    country: "Colombia",
+    city: "Bogota",
+  },
+];
+export const age = 70;
