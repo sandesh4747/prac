2027a3a8a5fd9c70f8c62508e33e3b986dc5dd95
sandesh4747 <sangitthapa328@gmail.com>
2025-03-19 17:11:45 +0545

    changes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -18,12 +18,12 @@ export default function Intro() {
       </div>
 
       {/* Text Section */}
-      <div className="space-y-2">
+      <div className="space-y-2 animate-slide-in-right">
         <h1 className="font-bold text-4xl">Hi, I am John</h1>
         <p className="text-pink-600 italic text-[18px]">
           Some Dev, Freelancer, Rounder
         </p>
-        <p className="text-[18px]">
+        <p className="text-[18px] ">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui ut
           voluptas numquam officiis beatae! Harum debitis mollitia ad sit autem!
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro atque,
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -13,8 +13,10 @@ import { Outlet } from "react-router";
 export default function Logo() {
   return (
     <div className="text-center mb-[30px]">
-      <h1 className="text-4xl font-semibold pb-4">Technologies I use</h1>
-      <div className="grid grid-cols-[1fr_1fr_1fr_1fr] justify-center items-center space-y-8 ">
+      <h1 className="text-4xl font-semibold pb-4 animate-slide-in-right">
+        Technologies I use
+      </h1>
+      <div className="grid grid-cols-[1fr_1fr_1fr_1fr] justify-center items-center space-y-8  ">
         <div className="transition-all duration-300 hover:text-red-500 hover:scale-110">
           <FontAwesomeIcon
             icon={faReact}
