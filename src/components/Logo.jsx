c72ab36e62062ec07bd4334c54e47deb633a1100
sandesh4747 <sangitthapa328@gmail.com>
2025-03-19 17:21:55 +0545

    changes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -12,11 +12,11 @@ import { Outlet } from "react-router";
 
 export default function Logo() {
   return (
-    <div className="text-center mb-[30px] animate-slide-in-right">
-      <h1 className="text-4xl font-semibold pb-4 animate-slide-in-right">
+    <div className="text-center mb-[30px] ">
+      <h1 className="text-4xl font-semibold pb-4 animate-slide-in-right ">
         Technologies I use
       </h1>
-      <div className="grid grid-cols-[1fr_1fr_1fr_1fr] justify-center items-center space-y-8  ">
+      <div className="grid grid-cols-[1fr_1fr_1fr_1fr] justify-center items-center space-y-8 animate-disappear-appear ">
         <div className="transition-all duration-300 hover:text-red-500 hover:scale-110">
           <FontAwesomeIcon
             icon={faReact}
