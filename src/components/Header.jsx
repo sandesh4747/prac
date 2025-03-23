ba0baf8e7d231bc479616aa5e4123f1b39f1c7b7
sandesh4747 <sangitthapa328@gmail.com>
2025-03-23 20:18:30 +0545

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,31 @@
-import React from "react";
-import { FaGithub } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaBars, FaGithub, FaTimes } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <div className="p-7 bg-gray-900 text-white">
       <div className="flex justify-between items-center">
-        <div className="flex gap-4 items-center justify-center">
+        <div className="flex gap-4 items-center justify-center ">
           <FaGithub className="h-[32px] w-auto" />
-          <nav className="space-x-7  ">
+
+          {/* Menu Icon */}
+          <button
+            onClick={toggleMenu}
+            className="max-sm:block focus:outline-none"
+          >
+            {isMenuOpen ? (
+              <FaTimes className="h-6 w-6" />
+            ) : (
+              <FaBars className="h-6 w-6" />
+            )}
+          </button>
+          {/* Desktop */}
+          <nav className="space-x-7 max-md:hidden">
             <NavLink className="hover:text-cyan-400 duration-300">
               Product
             </NavLink>
@@ -32,6 +49,34 @@ export default function Header() {
           </button>
         </div>
       </div>
+
+      {/* Mobile Nav */}
+      {isMenuOpen && (
+        <div className="max-sm:block mt-4">
+          <nav className="flex flex-col space-y-4">
+            <NavLink className="hover:text-cyan-400 duration-300">
+              Product
+            </NavLink>
+            <NavLink className="hover:text-cyan-400 duration-300">
+              Features
+            </NavLink>
+            <NavLink className="hover:text-cyan-400 duration-300">
+              Marketplace
+            </NavLink>
+            <NavLink className="hover:text-cyan-400 duration-300">
+              Company
+            </NavLink>
+          </nav>
+          <div className="mt-4 space-y-4">
+            <button className="w-full text-left hover:text-cyan-400 duration-300">
+              Log in
+            </button>
+            <button className="w-full bg-gray-500 py-[6px] px-[12px] rounded-[6px] cursor-pointer hover:bg-cyan-500 duration-300">
+              Start free trial
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
