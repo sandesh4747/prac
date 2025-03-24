58db5e4570e2d569b4bef55fe63023a575eafdf4
sandesh4747 <sangitthapa328@gmail.com>
2025-03-24 12:40:46 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RootLayout from "./components/RootLayout";
-import Home from "./pages/home/Home";
-import About from "./pages/about/About";
-import Contact from "./pages/contact/Contact";
-import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
 
 export default function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        { path: "about", element: <About /> },
-        { path: "contact", element: <Contact /> },
-      ],
-    },
-    {
-      path: "*",
-      element: <NotFound />,
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
