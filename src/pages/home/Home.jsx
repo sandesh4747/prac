5fe5db560826588824c47fc3ccbb4932540664e3
sandesh4747 <sangitthapa328@gmail.com>
2025-04-03 08:51:43 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,42 @@
+import axios from "axios";
 import { Formik } from "formik";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import Category from "./Category";
+import SearchItem from "./SearchItem";
 
 export default function Home() {
+  const nav = useNavigate();
+
+  const [data, setData] = useState();
+  const [load, setLoad] = useState(false);
+  const [search, setSearch] = useState(null);
+
+  const getData = async () => {
+    setLoad(true); /// calll
+    try {
+      if (search) {
+        const response = await axios.get(
+          "https://www.themealdb.com/api/json/v1/1/categories.php"
+        );
+        setData((prev) => response.data);
+      } else {
+        const response = await axios.get(
+          "https://www.themealdb.com/api/json/v1/1/categories.php"
+        ); //call
+        setData((prev) => response.data);
+      }
+
+      setLoad(false);
+    } catch (err) {
+      setLoad(false);
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, [search]);
+
   return (
     <div className="p-5">
       <Formik
@@ -9,8 +44,9 @@ export default function Home() {
           query: "",
         }}
         onSubmit={(val, { resetForm }) => {
+          setSearch(val.query);
           console.log(val);
-          resetForm();
+          // resetForm();
         }}
       >
         {({ handleChange, handleSubmit, values }) => {
@@ -31,7 +67,7 @@ export default function Home() {
               </div>
 
               <button
-                className="bg-black text-white px-4 py-2 rounded-sm text-sm"
+                className="bg-black text-white text-sm px-5 py-2 rounded-sm"
                 type="submit"
               >
                 Submit
@@ -40,6 +76,7 @@ export default function Home() {
           );
         }}
       </Formik>
+      {search ? <SearchItem data={data} /> : <Category data={data} />}
     </div>
   );
 }
