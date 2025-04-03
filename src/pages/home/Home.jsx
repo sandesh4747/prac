deb3ccb03cbcdf45b60268b963e2002b9fe356bc
sandesh4747 <sangitthapa328@gmail.com>
2025-04-03 08:27:14 +0545

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,30 +1,41 @@
 import { Formik } from "formik";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Home() {
   return (
-    <div className="p-4">
+    <div className="p-5">
       <Formik
         initialValues={{
-          username: "",
+          query: "",
         }}
-        onSubmit={(val) => {
+        onSubmit={(val, { resetForm }) => {
           console.log(val);
+          resetForm();
         }}
       >
-        {({ handleChange, handleSubmit, values, touched }) => {
+        {({ handleChange, handleSubmit, values }) => {
           return (
-            <form onSubmit={handleSubmit}>
-              <input
-                onChange={handleChange}
-                value={values.username}
-                className="border-2"
-                placeholder="username"
-                type="text"
-                name="username"
-              />
+            <form
+              onSubmit={handleSubmit}
+              className="space-y-2 flex gap-4 items-baseline"
+            >
+              <div>
+                <input
+                  className="border-2 border-gray-500 px-2 w-[300px] py-1"
+                  value={values.query}
+                  onChange={handleChange}
+                  type="text"
+                  name="query"
+                  placeholder="search for meals"
+                />
+              </div>
 
-              <button type="submit">Submit</button>
+              <button
+                className="bg-black text-white px-4 py-2 rounded-sm text-sm"
+                type="submit"
+              >
+                Submit
+              </button>
             </form>
           );
         }}
