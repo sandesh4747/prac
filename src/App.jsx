6856b4a9ab7ca956a9f978fdde441dd517972085
sandesh4747 <sangitthapa328@gmail.com>
2025-04-15 22:10:28 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Accordian from "./components/accordian/Accordian";
 import "./App.css";
+import RandomColor from "./components/random-color/RandomColor";
 
 export default function App() {
   return (
     <div className="App">
-      <Accordian />
+      {/* Accordian component */}
+      {/* <Accordian /> */}
+
+      {/* Random color component */}
+      <RandomColor />
     </div>
   );
 }
