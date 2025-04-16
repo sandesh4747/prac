8330f46e390d12494f32ab8bb951809a0a7e038b
sandesh4747 <sangitthapa328@gmail.com>
2025-04-16 21:57:05 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Accordian from "./components/accordian/Accordian";
 import "./App.css";
 import RandomColor from "./components/random-color/RandomColor";
 import StarRating from "./components/star-rating/StarRating";
+import ImageSlider from "./components/image-slider/ImageSlider";
 
 export default function App() {
   return (
@@ -14,7 +15,14 @@ export default function App() {
       {/* <RandomColor /> */}
 
       {/* Star Rating component */}
-      <StarRating noOfStars={10} />
+      {/* <StarRating noOfStars={10} /> */}
+
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        limit={"10"}
+        page={"1"}
+      />
     </div>
   );
 }
