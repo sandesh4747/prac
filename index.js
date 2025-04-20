0ecd0f31922e2869b8e031a56119b64fe197514d
sandesh4747 <sangitthapa328@gmail.com>
2025-04-20 08:59:08 +0545

    changes

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("hello jee1");
+// setImmediate(() => {
+//   console.log("immediate");
+// });
+setTimeout(() => {
+  for (let i = 0; i <= 100000000000; i++) {}
+}, 1000);
+
+console.log("hello jee2");
